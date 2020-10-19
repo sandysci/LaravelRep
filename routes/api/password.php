cf2d3a02277936432cbdfdd2fd0e503cb2adc323
Victor Alagwu <victoralagwu@gmail.com>
2020-10-19 12:04:44 +0100

    Fix

--- routes/api/password.php
+++ routes/api/password.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\v1\Auth\ForgotPasswordController;
+use App\Http\Controllers\API\v1\Auth\PasswordResetController;
 
 Route::post('forgot', [ForgotPasswordController::class, 'create']);
 Route::put('reset', [PasswordResetController::class, 'reset']);
